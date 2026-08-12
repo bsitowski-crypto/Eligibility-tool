7f8e8316276613f79d57ca7af6a27e941f93a52c
bsitowski-crypto <bsitowski@gmail.com>
2026-08-11 19:25:56 -0700

    Refresh cache after donor open hotfix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="solvita-planner-v9-10-9-transport-location1";
-const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icons/icon.svg","./app/data-1.b64","./app/data-2.b64","./app/data-3.b64","./cloud-sync.js","./collaboration.js","./compact-menu.js","./directory-recovery.js","./transport-planner.js","./transport-location.js"];
+const CACHE="solvita-planner-v9-10-9-hotfix-donor-open1";
+const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icons/icon.svg","./app/data-1.b64","./app/data-2.b64","./app/data-3.b64","./cloud-sync.js","./collaboration.js","./compact-menu.js","./directory-recovery.js","./transport-planner.js"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;const url=new URL(event.request.url);if(event.request.mode==="navigate"){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put("./index.html",copy));return response}).catch(()=>caches.match("./index.html")));return}if(url.origin===self.location.origin){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))}});
